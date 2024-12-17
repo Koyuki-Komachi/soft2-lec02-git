86f5142f140506d374fed812c52fab1dd9f4cde8
Koyuki-Komachi <ikeda-kosuke700@g.ecc.u-tokyo.ac.jp>
2024-12-17 05:31:48 +0000

    added getopt to input standard deviation

--- my_darts2.c
+++ my_darts2.c
@@ -1,6 +1,8 @@
 #include "darts.h"
+#include <bits/getopt_core.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <stdbool.h>
 #include <unistd.h>
@@ -8,6 +10,18 @@
 
 #define RADIUS 20
 
+/*
+darts.hの中で、PointとBoardは次のように定義されています。
+typedef struct point{
+    double x;
+    double y;
+} Point;
+
+typedef struct board{
+    char space[2*RADIUS + 1][4 * RADIUS + 3];
+    double radius;
+} Board;
+*/
 
 // 初期化して円を表示する
 void my_init_board(Board *b) {
@@ -144,24 +158,260 @@ void my_print_point(Point p) {
     printf("(%f %f)", p.x, p.y);
 }
 
+// ユーザの入力によって、正規分布の中心を指定する
+Point gauss_various_center(char *s) {
+    Point center = {.x = 0, .y = 0};
+    if (s == NULL) {
+        printf("center designation is missing\n");
+        exit(EXIT_FAILURE);
+    }
+    // ブルを狙った場合
+    else if (strcmp(s, "B") == 0) {
+        center.x = 0;
+        center.y = 0;
+    }
+    // トリプルを狙っ場合
+    else if (strcmp(s, "T6") == 0) {
+        center.x = 12;
+        center.y = 0;
+    }else if (strcmp(s, "T10") == 0) {
+        center.x = 12 * cos(M_PI / 10);
+        center.y = 12 * sin(M_PI / 10);
+    }else if (strcmp(s, "T15") == 0) {
+        center.x = 12 * cos(M_PI * 2 / 10);
+        center.y = 12 * sin(M_PI * 2 / 10);
+    }else if (strcmp(s, "T2") == 0) {
+        center.x = 12 * cos(M_PI * 3 / 10);
+        center.y = 12 * sin(M_PI * 3 / 10);
+    }else if (strcmp(s, "T17") == 0) {
+        center.x = 12 * cos(M_PI * 4 / 10);
+        center.y = 12 * sin(M_PI * 4 / 10);
+    }else if (strcmp(s, "T3") == 0) {
+        center.x = 12 * cos(M_PI * 5 / 10);
+        center.y = 12 * sin(M_PI * 5 / 10);
+    }else if (strcmp(s, "T19") == 0) {
+        center.x = 12 * cos(M_PI * 6 / 10);
+        center.y = 12 * sin(M_PI * 6 / 10);
+    }else if (strcmp(s, "T7") == 0) {
+        center.x = 12 * cos(M_PI * 7 / 10);
+        center.y = 12 * sin(M_PI * 7 / 10);
+    }else if (strcmp(s, "T16") == 0) {
+        center.x = 12 * cos(M_PI * 8 / 10);
+        center.y = 12 * sin(M_PI * 8 / 10);
+    }else if (strcmp(s, "T8") == 0) {
+        center.x = 12 * cos(M_PI * 9 / 10);
+        center.y = 12 * sin(M_PI * 9 / 10);
+    }else if (strcmp(s, "T11") == 0) {
+        center.x = 12 * cos(M_PI);
+        center.y = 12 * sin(M_PI);
+    }else if (strcmp(s, "T14") == 0) {
+        center.x = 12 * cos(M_PI * 11 / 10);
+        center.y = 12 * sin(M_PI * 11 / 10);
+    }else if (strcmp(s, "T9") == 0) {
+        center.x = 12 * cos(M_PI * 12 / 10);
+        center.y = 12 * sin(M_PI * 12 / 10);
+    }else if (strcmp(s, "T12") == 0) {
+        center.x = 12 * cos(M_PI * 13 / 10);
+        center.y = 12 * sin(M_PI * 13 / 10);
+    }else if (strcmp(s, "T5") == 0) {
+        center.x = 12 * cos(M_PI * 14 / 10);
+        center.y = 12 * sin(M_PI * 14 / 10);
+    }else if (strcmp(s, "T20") == 0) {
+        center.x = 12 * cos(M_PI * 15 / 10);
+        center.y = 12 * sin(M_PI * 15 / 10);
+    }else if (strcmp(s, "T1") == 0) {
+        center.x = 12 * cos(M_PI * 16 / 10);
+        center.y = 12 * sin(M_PI * 16 / 10);
+    }else if (strcmp(s, "T18") == 0) {
+        center.x = 12 * cos(M_PI * 17 / 10);
+        center.y = 12 * sin(M_PI * 17 / 10);
+    }else if (strcmp(s, "T4") == 0) {
+        center.x = 12 * cos(M_PI * 18 / 10);
+        center.y = 12 * sin(M_PI * 18 / 10);
+    }else if (strcmp(s, "T13") == 0) {
+        center.x = 12 * cos(M_PI * 19 / 10);
+        center.y = 12 * sin(M_PI * 19 / 10);
+    }
+    // ダブルを狙った場合
+    else if (strcmp(s, "D6") == 0) {
+        center.x = 19;
+        center.y = 0;
+    }else if (strcmp(s, "D10") == 0) {
+        center.x = 19 * cos(M_PI / 10);
+        center.y = 19 * sin(M_PI / 10);
+    }else if (strcmp(s, "D15") == 0) {
+        center.x = 19 * cos(M_PI * 2 / 10);
+        center.y = 19 * sin(M_PI * 2 / 10);
+    }else if (strcmp(s, "D2") == 0) {
+        center.x = 19 * cos(M_PI * 3 / 10);
+        center.y = 19 * sin(M_PI * 3 / 10);
+    }else if (strcmp(s, "D17") == 0) {
+        center.x = 19 * cos(M_PI * 4 / 10);
+        center.y = 19 * sin(M_PI * 4 / 10);
+    }else if (strcmp(s, "D3") == 0) {
+        center.x = 19 * cos(M_PI * 5 / 10);
+        center.y = 19 * sin(M_PI * 5 / 10);
+    }else if (strcmp(s, "D19") == 0) {
+        center.x = 19 * cos(M_PI * 6 / 10);
+        center.y = 19 * sin(M_PI * 6 / 10);
+    }else if (strcmp(s, "D7") == 0) {
+        center.x = 19 * cos(M_PI * 7 / 10);
+        center.y = 19 * sin(M_PI * 7 / 10);
+    }else if (strcmp(s, "D16") == 0) {
+        center.x = 19 * cos(M_PI * 8 / 10);
+        center.y = 19 * sin(M_PI * 8 / 10);
+    }else if (strcmp(s, "D8") == 0) {
+        center.x = 19 * cos(M_PI * 9 / 10);
+        center.y = 19 * sin(M_PI * 9 / 10);
+    }else if (strcmp(s, "D11") == 0) {
+        center.x = 19 * cos(M_PI);
+        center.y = 19 * sin(M_PI);
+    }else if (strcmp(s, "D14") == 0) {
+        center.x = 19 * cos(M_PI * 11 / 10);
+        center.y = 19 * sin(M_PI * 11 / 10);
+    }else if (strcmp(s, "D9") == 0) {
+        center.x = 19 * cos(M_PI * 12 / 10);
+        center.y = 19 * sin(M_PI * 12 / 10);
+    }else if (strcmp(s, "D12") == 0) {
+        center.x = 19 * cos(M_PI * 13 / 10);
+        center.y = 19 * sin(M_PI * 13 / 10);
+    }else if (strcmp(s, "D5") == 0) {
+        center.x = 19 * cos(M_PI * 14 / 10);
+        center.y = 19 * sin(M_PI * 14 / 10);
+    }else if (strcmp(s, "D20") == 0) {
+        center.x = 19 * cos(M_PI * 15 / 10);
+        center.y = 19 * sin(M_PI * 15 / 10);
+    }else if (strcmp(s, "D1") == 0) {
+        center.x = 19 * cos(M_PI * 16 / 10);
+        center.y = 19 * sin(M_PI * 16 / 10);
+    }else if (strcmp(s, "D18") == 0) {
+        center.x = 19 * cos(M_PI * 17 / 10);
+        center.y = 19 * sin(M_PI * 17 / 10);
+    }else if (strcmp(s, "D4") == 0) {
+        center.x = 19 * cos(M_PI * 18 / 10);
+        center.y = 19 * sin(M_PI * 18 / 10);
+    }else if (strcmp(s, "D13") == 0) {
+        center.x = 19 * cos(M_PI * 19 / 10);
+        center.y = 19 * sin(M_PI * 19 / 10);
+    }
+    // シングルを狙った場合
+    else if (strcmp(s, "S6") == 0) {
+        center.x = 10;
+        center.y = 0;
+    }else if (strcmp(s, "D10") == 0) {
+        center.x = 10 * cos(M_PI / 10);
+        center.y = 10 * sin(M_PI / 10);
+    }else if (strcmp(s, "D15") == 0) {
+        center.x = 10 * cos(M_PI * 2 / 10);
+        center.y = 10 * sin(M_PI * 2 / 10);
+    }else if (strcmp(s, "D2") == 0) {
+        center.x = 10 * cos(M_PI * 3 / 10);
+        center.y = 10 * sin(M_PI * 3 / 10);
+    }else if (strcmp(s, "D17") == 0) {
+        center.x = 10 * cos(M_PI * 4 / 10);
+        center.y = 10 * sin(M_PI * 4 / 10);
+    }else if (strcmp(s, "D3") == 0) {
+        center.x = 10 * cos(M_PI * 5 / 10);
+        center.y = 10 * sin(M_PI * 5 / 10);
+    }else if (strcmp(s, "D19") == 0) {
+        center.x = 10 * cos(M_PI * 6 / 10);
+        center.y = 10 * sin(M_PI * 6 / 10);
+    }else if (strcmp(s, "D7") == 0) {
+        center.x = 10 * cos(M_PI * 7 / 10);
+        center.y = 10 * sin(M_PI * 7 / 10);
+    }else if (strcmp(s, "D16") == 0) {
+        center.x = 10 * cos(M_PI * 8 / 10);
+        center.y = 10 * sin(M_PI * 8 / 10);
+    }else if (strcmp(s, "D8") == 0) {
+        center.x = 10 * cos(M_PI * 9 / 10);
+        center.y = 10 * sin(M_PI * 9 / 10);
+    }else if (strcmp(s, "D11") == 0) {
+        center.x = 10 * cos(M_PI);
+        center.y = 10 * sin(M_PI);
+    }else if (strcmp(s, "D14") == 0) {
+        center.x = 10 * cos(M_PI * 11 / 10);
+        center.y = 10 * sin(M_PI * 11 / 10);
+    }else if (strcmp(s, "D9") == 0) {
+        center.x = 10 * cos(M_PI * 12 / 10);
+        center.y = 10 * sin(M_PI * 12 / 10);
+    }else if (strcmp(s, "D12") == 0) {
+        center.x = 10 * cos(M_PI * 13 / 10);
+        center.y = 10 * sin(M_PI * 13 / 10);
+    }else if (strcmp(s, "D5") == 0) {
+        center.x = 10 * cos(M_PI * 14 / 10);
+        center.y = 10 * sin(M_PI * 14 / 10);
+    }else if (strcmp(s, "D20") == 0) {
+        center.x = 10 * cos(M_PI * 15 / 10);
+        center.y = 10 * sin(M_PI * 15 / 10);
+    }else if (strcmp(s, "D1") == 0) {
+        center.x = 10 * cos(M_PI * 16 / 10);
+        center.y = 10 * sin(M_PI * 16 / 10);
+    }else if (strcmp(s, "D18") == 0) {
+        center.x = 10 * cos(M_PI * 17 / 10);
+        center.y = 10 * sin(M_PI * 17 / 10);
+    }else if (strcmp(s, "D4") == 0) {
+        center.x = 10 * cos(M_PI * 18 / 10);
+        center.y = 10 * sin(M_PI * 18 / 10);
+    }else if (strcmp(s, "D13") == 0) {
+        center.x = 10 * cos(M_PI * 19 / 10);
+        center.y = 10 * sin(M_PI * 19 / 10);
+    }
+    // 上記以外の文字列が打ち込まれた場合
+    else {
+        printf("center designation is invalid\n");
+        exit(EXIT_FAILURE);
+    }
+    return center;
+}
+
 int main(int argc, char **argv){
     Board board;
-
     srand((unsigned int)time(NULL));
-
     my_init_board(&board);
 
-    // 3回投げる
-    for (int i = 1 ; i <= 3 ; i++){
-        Point p = my_iso_gauss_rand((Point){.x = 0, .y = 0}, 15.0);
+    // -vオプションから標準偏差を読み取る
+    char *stddev = NULL;
+    int opt;
+    while ((opt = getopt(argc, argv, "v:")) != -1) {
+        switch (opt) {
+            case 'v':
+                stddev = optarg;
+                break;
+            case '?':
+                if (optopt == 'v') {
+                    fprintf(stderr, "Option -%c requires a standard deviation.\n", optopt);
+                }else {
+                    fprintf(stderr, "Unknown option -%c.\n", optopt);
+                }
+                return 1;
+        }
+    }
+    if (stddev == NULL) {
+        fprintf(stderr, "Standard deviation (-v option) is required.\n");
+        return 1;
+    }
+    if (argc - optind < 3) {
+        fprintf(stderr, "Three throwing targets are requires.\n");
+        return 1;
+    }
 
-        my_plot_throw(&board,p,i);
+    // 3回投げる
+    char sum_score = 0;
+    for (int i = 0 ; i < 3 ; i++){
+        // (i + 1)回目の狙いをarg[optind + i]に書き込む
+        Point center = gauss_various_center(argv[optind + i]);
+        Point p = my_iso_gauss_rand(center, atoi(stddev));
+
+        my_plot_throw(&board,p,i + 1);
         my_print_board(&board);
         printf("-------\n");
         my_print_point(p);
         if (!my_is_valid_point(&board, p)) printf(" miss!\n");
-        printf(" %d回目のスコアは%dです\n", i, score(p));
+        printf(" %d回目のスコアは%dです\n", i + 1, score(p));
+        sum_score += score(p);
         sleep(1);
     }
+    printf("\n");
+    printf("合計のスコアは%dです\n", sum_score);
     return 0;
 }
